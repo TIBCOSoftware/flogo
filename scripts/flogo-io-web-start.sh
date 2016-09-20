c3294b693b8d6f833776862815dc17fabb4a3f12
Pranab Dhar <pdhar@tibco.com>
2016-09-20 15:41:32 -0700

    Fix script issues

--- scripts/flogo-io-web-start.sh
+++ scripts/flogo-io-web-start.sh
@@ -19,14 +19,14 @@ checkDependencies() {
         cat >&2 <<-'EOF'
         Error: this installer needs "docker-compose"
         We are unable to find "docker-compose" to make this happen.
-        EOF
+EOF
         exit 1 
     fi
     if ! command_exists jq ; then 
         cat >&2 <<-'EOF'
         Error: this installer needs "jq"
         We are unable to find "jq" to make this happen.
-        EOF
+EOF
         exit 1 
     fi
     if command_exists curl ; then
@@ -37,7 +37,7 @@ checkDependencies() {
         cat >&2 <<-'EOF'
         Error: this installer needs "curl" or "wget"
         We are unable to find either "curl" or "wget" available to make this happen.
-        EOF
+EOF
         exit 1 
     fi
 }
@@ -46,36 +46,37 @@ getLatestRelease() {
     owner=${1:-"${GITHUB_OWNER}"}
     repo=${2:-"${GITHUB_REPO}"}
     github_url="https://api.github.com/repos/${owner}/${repo}/releases/latest"
-    tag_name=$(curl -s ${github_url} | jq -r '.tag_name')
+    tag_name=$(curl -s "${github_url}" | jq -r '.tag_name')
     if [ -n "${tag_name}" ]; then 
         cat >&2 <<-'EOF'
         Error: "Latest" release not found at https://github.com/TIBCOSoftware/flogo-web/releases
-        EOF
+EOF
+        exit 1
     fi
-    asset_urls=$(curl -s ${github_url} | jq -r '.assets[] | .browser_download_url' | grep "compose")
+    asset_urls=$(curl -s "${github_url}" | jq -r '.assets[] | .browser_download_url' | grep "compose")
     for i in ${asset_urls}; do
     echo "Downloading ... ${i}"
-    download_file ${i}
+    download_file "${i}"
     done
 }
 
 
 download_file() {
-    local file_url="${1}"
+    file_url="${1}"
     if command_exists curl ; then
-        curl -fsSLO ${file_url}
+        curl -fsSLO "${file_url}"
     elif command_exists wget; then
-        wget --no-check-certificate -q -N -O ${file_url##*/}  ${file_url}
+        wget --no-check-certificate -q -N -O "${file_url##*/}"  "${file_url}"
     fi
 }
 
 run_command() {
     checkDependencies
     getLatestRelease "TIBCOSoftware" "flogo"
-    chmod +x ${script_root}/docker-compose-start.sh
-    if command_exists docker-compose && [ -r ${script_root}/docker-compose.yml ] && [ -x ${script_root}/docker-compose-start.sh ]; then
-        echo docker-compose -f ${script_root}/docker-compose.yml up
-        echo docker-compose rm -f
+    chmod +x "${script_root}/docker-compose-start.sh"
+    if command_exists docker-compose && [ -r "${script_root}/docker-compose.yml" ] && [ -x "${script_root}/docker-compose-start.sh" ]; then
+        docker-compose -f "${script_root}/docker-compose.yml" up
+        docker-compose rm -f
     fi
 }
 # wrapped up in a function so that we have some protection against only getting
