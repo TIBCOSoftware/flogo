65768aac2ae76b3ca9a2bcbbb872dec04b31f24f
retgits <lstigter@gmail.com>
2018-07-23 18:54:56 -0700

    Updates to doc generation [WIP]

--- build-docs.sh
+++ build-docs.sh
@@ -1,41 +1,106 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-echo "Prepare docs..."
-echo "Update contributing page"
-cp CONTRIBUTING.md docs/content/contributing/contributing.md
-sed -i '1d' docs/content/contributing/contributing.md
-sed -i '1i ---' docs/content/contributing/contributing.md
-sed -i '1i weight: 9010' docs/content/contributing/contributing.md
-sed -i '1i title: Contributing to Project Flogo' docs/content/contributing/contributing.md
-sed -i '1i ---' docs/content/contributing/contributing.md
+# Description: Build script for Project Flogo documentation
+# Author: retgits <https://github.com/retgits>
+# Last Updated: 2018-07-23
 
-echo "Updating the frontpage"
-cp README.md docs/content/introduction/_index.md
-sed -i '1,4d' docs/content/introduction/_index.md
-sed -i '5,18d' docs/content/introduction/_index.md
-sed -i '1i ---' docs/content/introduction/_index.md
-sed -i '1i pre: "<i class=\\"fa fa-home\\" aria-hidden=\\"true\\"></i> "' docs/content/introduction/_index.md
-sed -i '1i weight: 1000' docs/content/introduction/_index.md
-sed -i '1i title: Introduction' docs/content/introduction/_index.md
-sed -i '1i ---' docs/content/introduction/_index.md
-sed -i 's#images/flogo-web2.gif#https://raw.githubusercontent.com/TIBCOSoftware/flogo/master/images/flogo-web2.gif#g' docs/content/introduction/_index.md
-sed -i 's#images/flogo-cli.gif#https://raw.githubusercontent.com/TIBCOSoftware/flogo/master/images/flogo-cli.gif#g' docs/content/introduction/_index.md
+#--- Variables ---
+HUGO_VERSION=0.45
 
-echo "Getting the docs for the activities and triggers"
-git clone https://github.com/TIBCOSoftware/flogo-contrib
-for i in `find flogo-contrib/activity -name \*.md` ; do filename=$(basename $(dirname $i)); cp $i docs/content/development/webui/activities/$filename.md; done;
-for i in `find flogo-contrib/trigger -name \*.md` ; do filename=$(basename $(dirname $i)); cp $i docs/content/development/webui/triggers/$filename.md; done;
-rm -rf flogo-contrib
+#--- Download and install prerequisites ---
+prerequisites() {
+    wget -O hugo.tar.gz https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz
+    mkdir -p hugobin
+    tar -xzvf hugo.tar.gz -C ./hugobin
+    mv ./hugobin/hugo $HOME/gopath/bin
+    rm hugo.tar.gz && rm -rf ./hugobin
+}
 
-echo "Getting the docs for the commandline tools"
-curl -o docs/content/flogo-cli/flogo-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogo-cli.md
-curl -o docs/content/flogo-cli/flogodevice-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogodevice-cli.md
-curl -o docs/content/flogo-cli/flogogen-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogogen-cli.md
-curl -o docs/content/flogo-cli/tools-overview.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/tools-overview.md
+#--- Get external docs ---
+ext_docs() {
+    echo "Getting the docs for the activities and triggers"
+    git clone https://github.com/TIBCOSoftware/flogo-contrib
+    for i in `find flogo-contrib/activity -name \*.md` ; do filename=$(basename $(dirname $i)); cp $i docs/content/development/webui/activities/$filename.md; done;
+    for i in `find flogo-contrib/trigger -name \*.md` ; do filename=$(basename $(dirname $i)); cp $i docs/content/development/webui/triggers/$filename.md; done;
+    rm -rf flogo-contrib
 
-echo "Build docs site..."
-cd docs && hugo
-cd ../showcases && hugo
-mv public ../docs/public/showcases
-cd ../docs
-cd public && ls -alh
+    echo "Getting the docs for the commandline tools"
+    curl -o docs/content/flogo-cli/flogo-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogo-cli.md
+    curl -o docs/content/flogo-cli/flogodevice-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogodevice-cli.md
+    curl -o docs/content/flogo-cli/flogogen-cli.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/flogogen-cli.md
+    curl -o docs/content/flogo-cli/tools-overview.md https://raw.githubusercontent.com/TIBCOSoftware/flogo-cli/master/docs/tools-overview.md
+
+    echo "Getting interactive labs"
+    git clone https://github.com/retgits/katacoda-scenarios
+    for i in `find katacoda-scenarios -name \*interactive-lab.md` ; do filename=$(basename $i); cp $i docs/content/labs/$filename.md; title=`echo | awk 'NR==2 {print;exit}' docs/content/labs/$filename.md`; echo "* [${title:7}](./$filename)" >> docs/content/labs/_index.md; done;
+    rm -rf katacoda-scenarios
+}
+
+#--- Update contributions page ---
+update_page_contrib() {
+    echo "Update contributing page"
+    cp CONTRIBUTING.md docs/content/contributing/contributing.md
+    sed -i '1d' docs/content/contributing/contributing.md
+    sed -i '1i ---' docs/content/contributing/contributing.md
+    sed -i '1i weight: 9010' docs/content/contributing/contributing.md
+    sed -i '1i title: Contributing to Project Flogo' docs/content/contributing/contributing.md
+    sed -i '1i ---' docs/content/contributing/contributing.md
+}
+
+#--- Update introduction page ---
+update_page_introduction() {
+    cp README.md docs/content/introduction/_index.md
+    sed -i '1,4d' docs/content/introduction/_index.md
+    sed -i '5,18d' docs/content/introduction/_index.md
+    sed -i '1i ---' docs/content/introduction/_index.md
+    sed -i '1i pre: "<i class=\\"fa fa-home\\" aria-hidden=\\"true\\"></i> "' docs/content/introduction/_index.md
+    sed -i '1i weight: 1000' docs/content/introduction/_index.md
+    sed -i '1i title: Introduction' docs/content/introduction/_index.md
+    sed -i '1i ---' docs/content/introduction/_index.md
+    sed -i 's#images/flogo-web2.gif#https://raw.githubusercontent.com/TIBCOSoftware/flogo/master/images/flogo-web2.gif#g' docs/content/introduction/_index.md
+    sed -i 's#images/flogo-cli.gif#https://raw.githubusercontent.com/TIBCOSoftware/flogo/master/images/flogo-cli.gif#g' docs/content/introduction/_index.md
+}
+
+#--- Update page ---
+update_page() {
+    case "$1" in
+        "contributing")
+            update_page_contrib
+            ;;
+        "introduction")
+            update_page_introduction
+            ;;
+        *)
+            echo "Updating all pages"
+            update_page_contrib
+            update_page_introduction
+    esac
+}
+
+#--- Execute build ---
+build() {
+    echo "Build docs site..."
+    cd docs && hugo
+    cd ../showcases && hugo
+    mv public ../docs/public/showcases
+    cd ../docs
+    cd public && ls -alh
+}
+
+
+case "$1" in 
+    "prerequisites")
+        prerequisites
+        ;;
+    "ext-docs")
+        ext_docs
+        ;;
+    "update-page")
+        update_page $2
+        ;;
+    "build")
+        build
+        ;;
+    *)
+        echo "The target {$1} you want to execute doesn't exist"
+esac
--- docs/content/labs/_index.md
+++ docs/content/labs/_index.md
@@ -7,4 +7,10 @@ pre: "<i class=\"fa fa-flask\" aria-hidden=\"true\"></i> "
 
 # Labs
 
-After getting up and running you might want to try your hand at some more advanced labs and tutorials.
+After getting up and running you might want to try your hand at some more advanced labs and tutorials. We have text-based labs that will get you up and running using tutorials on this website:
+
+* [Deploying to Kubernetes](./kubernetes-demo)
+* [Deploying using the Serverless Framework](./serverless)
+
+We also have a bunch of interactive labs, guiding you through the different steps without having to install anything!
+
